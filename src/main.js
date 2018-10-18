ccedbee2a3936cb02b462fcf242d201b93aa349c
johnsonS <879917302@qq.com>
2018-10-18 21:34:03 +0800

    vue init finished

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css' // 样式重置
+import './assets/styles/border.css' // 移动端1px解决
 
 Vue.config.productionTip = false
+fastClick.attach(document.body) // 解决移动端300ms延迟
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>HOME</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
